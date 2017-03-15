17874b6916d7038fce5bfccdcd591f18bac7a2a6
LCG_GIT <1542102449@qq.com>
2017-03-15 15:47:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cglin/layoutdemo3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cglin.layoutdemo3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
